9b0c484d84fbc4ef7ee2bb3941e5da930841e9f2
Mohammed Baba <mohammedbaba1505@gmail.com>
2025-12-25 15:51:36 +0100

    integrate cart state and navigation hooks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import { useMemo, useState } from 'react'
+import { Link, NavLink, useLocation, useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const Navbar = () => {
+  
   return (
     <div>Navbar</div>
   )
