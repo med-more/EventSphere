da07f516f90bf885174889a56e2535665b55f789
med-more <mbfmz20@gmail.com>
2025-12-30 00:44:40 +0100

    initialize state for events, form, and modal in AdminProducts

--- src/pages/admin/AdminProducts.jsx
+++ src/pages/admin/AdminProducts.jsx
@@ -4,6 +4,18 @@ import { getEvents, createEvent, updateEvent, deleteEvent } from '../../api/axio
 import { uploadImage } from '../../api/cloudinary';
 
 const AdminProducts = () => {
+  const [events, setEvents] = useState([]);
+  const [ showModal, setShowModal ] = useState(false);
+  const [ editingEvent, setEditingEvent ] = useState(null);
+  const [uploading, setUploading] = useState(false);
+  const [formData, setFormData] = useState({
+        name: '',
+        description: '',
+        category: 'Musique',
+        image: '',
+        price: '',
+        date: '',
+    });
   return (
     <div>AdminProducts</div>
   )
