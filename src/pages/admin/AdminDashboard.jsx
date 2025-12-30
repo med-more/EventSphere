ce823556cffb573477618d3ecd85d4d3491d9090
med-more <mbfmz20@gmail.com>
2025-12-30 14:02:04 +0100

    create AdminDashboard component scaffold

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { getEvents, getOrders } from '../../api/axios';
+
 
 const AdminDashboard = () => {
   return (
