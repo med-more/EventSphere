aae78269c16fd0a715e749bb06de4eb63f996002
med-more <mbfmz20@gmail.com>
2025-12-30 14:14:44 +0100

    initialize dashboard state and navigation

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -4,6 +4,13 @@ import { getEvents, getOrders } from '../../api/axios';
 
 
 const AdminDashboard = () => {
+  const [ stats, setStats ] = useState({
+        totalEvents: 0,
+        totalOrders: 0,
+        totalRevenue: 0,
+  });
+
+  const navigate = useNavigate();
   return (
     <div>AdminDashboard</div>
   )
