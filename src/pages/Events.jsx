f6c697dbd715621b02e134d51df9c98e103d1b41
med-more <mbfmz20@gmail.com>
2025-12-31 12:01:15 +0100

    setup Events page state, filters, and dependencies

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
+import { getEvents } from '../api/axios';
+import { addToCart } from '../features/cart/cartSlice';
+import EventCard from '../components/EventCard';
+import toast from 'react-hot-toast';
 
 const Events = () => {
+  const [events, setEvents] = useState([]);
+  const [ filteredEvents, setFilteredEvents ] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('All');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  const [searchParams] = useSearchParams();
+
+    const categories = ['All', 'Musique', 'Art', 'Spectacle', 'Football'];
   return (
     <div>Events</div>
   )
