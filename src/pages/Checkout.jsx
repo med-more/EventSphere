3552cb7b67a1789f13168452ab4016853cbc0030
med-more <mbfmz20@gmail.com>
2026-01-02 15:34:45 +0100

    create initial Checkout component

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { selectCartItems, selectCartTotal, clearCart } from '../features/cart/cartSlice';
+import { createOrder, getStats, updateStats } from '../api/axios';
+import toast from 'react-hot-toast';
+
 
 const Checkout = () => {
   return (
