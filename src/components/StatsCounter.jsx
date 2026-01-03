95a116aab20d58442eb03ff843e2e71eacf611fb
med-more <mbfmz20@gmail.com>
2026-01-03 13:09:11 +0100

    add animated counter logic to StatsCounter

--- src/components/StatsCounter.jsx
+++ src/components/StatsCounter.jsx
@@ -1,7 +1,28 @@
 import { useEffect, useState } from 'react';
 import { getStats } from '../api/axios';
 
-const StatsCounter = () => {
+const StatsCounter = ({ value }) => {
+    const [count, setCount] = useState(0);
+
+    useEffect(() =>{
+        let start = 0;
+        const end = parseInt(value, 10);
+        if(start === end) return;
+
+        let totalDuration = 2000;
+        let incrementTime = (totalDuration / end) * 5;
+
+        if(end > 1000) incrementTime = 10;
+
+        let timer = setInterval(() =>{
+            start += Math.ceil(end / 100);
+            if(start > end) start = end;
+            setCount(start);
+            if(start === end) clearInterval(timer);
+        }, incrementTime);
+
+        return () => clearInterval(timer);
+    }, [value]);
   return (
     <div>StatsCounter</div>
   )
