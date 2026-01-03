fbdfcd9adea2bce5d59251ebfb42431876d1585d
med-more <mbfmz20@gmail.com>
2026-01-04 00:14:12 +0100

    initialize Testimonials page with dependencies

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getTestimonials } from '../api/axios';
+import toast from 'react-hot-toast';
+
+
 
 const Testimonials = () => {
   return (
