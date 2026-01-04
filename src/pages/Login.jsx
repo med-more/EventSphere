5a6ad3946495bfc5d6ea221a883ba61cedc46366
med-more <mbfmz20@gmail.com>
2026-01-04 16:29:14 +0100

    initialize login page component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import toast from 'react-hot-toast';
+
 
 const Login = () => {
   return (
