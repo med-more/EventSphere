5465dc9cb4c9726d614f3ccb48a3500cfa042612
med-more <mbfmz20@gmail.com>
2026-01-04 16:35:20 +0100

    handle login form input changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,6 +8,14 @@ const Login = () => {
     username: '',
     password: '',
   });
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
     <div>Login</div>
   )
