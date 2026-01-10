7623f715f2b5b4736c53d66425efd7c48a907f4d
med-more <mbfmz20@gmail.com>
2026-01-10 20:14:18 +0100

    create basic Home component with imports for events, categories, stats and testimonials

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
-
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { getEvents, getCategories } from '../api/axios';
+import StatsCounter from '../components/StatsCounter';
+import Testimonials from '../components/Testimonials';
 const Home = () => {
   return (
     <div>Home</div>
