21a91e6ad9e33f96a3e7350ac6ed1abaca8322c4
med-more <mbfmz20@gmail.com>
2026-01-10 20:43:44 +0100

    add state for contact form fields

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,6 +3,11 @@ import { createContact } from '../api/axios';
 import toast from 'react-hot-toast';
 
 const Contact = () => {
+   const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
   return (
     <div>Contact</div>
   )
